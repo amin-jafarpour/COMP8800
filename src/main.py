d1364e3c1dcf8118898f7ddaf69d937112235d55
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-01-18 13:17:09 -0800

    added arp_scan()

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import subprocess
 
 
 def get_cidr(network_adapter='wlp164s0'):
+    #ip addr show wlp164s0  | grep -oP 'inet \K[\d.]+/\d+'
     ip_result = subprocess.run(['ip', 'addr', 'show', 'wlp164s0'], capture_output=True, text=True)
 
     if ip_result.returncode == 0:
@@ -16,17 +17,27 @@ def get_cidr(network_adapter='wlp164s0'):
             capture_output=True
         )
         if grep_result.returncode == 0:
-            print(grep_result.stdout.strip())
+            return grep_result.stdout.strip()
         else:
             print(f"Grep Error: {grep_result.stderr}")
+            return None
     else:
         print(f"IP Command Error: {ip_result.stderr}")
+        return None
 
 
 
 
 
-#ip addr show wlp164s0  | grep -oP 'inet \K[\d.]+/\d+'
 
-def arp_scan():
-    pass
+
+def arp_scan(cidr):
+    answered, unanswered = arping(cidr)
+    for sent, received in answered:
+        print(f"IP: {received.psrc}  MAC: {received.hwsrc}")
+
+
+
+
+
+arp_scan(get_cidr())
