844c963be44b8a950ad01d65729f911e8f7fc775
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-01-30 18:42:37 -0800

    made doc dir, progress_log.md and modified network_discovery.py

---
+++ doc/progress_log.md
@@ -0,0 +1,9 @@
+## Progress Report
+
+
+| Feature               | Status     | Explanation           |
+|:---------------------:|:----------:|:---------------------:|
+| Network Discovery     | Complete   | network_discovery.py  |
+| Data 4                | Data 5     | Data 6                |
+| Data 7                | Data 8     | Data 9                |
+
--- src/network_discovery.py
+++ src/network_discovery.py
@@ -3,50 +3,34 @@ import time
 
 
 
-def packet_handler(pkt):
-
+def packet_handler(iface, count, interval=0.2):
     """
     """
-    discovered_networks = []
-    # check if Beacon Frame or Prob Response Frame
-    if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
-        # Extract the MAC address of the AP (BSSID)
-
-        bssid = pkt[Dot11].addr2
-        ssid = None
-        # Dot11Elt with ID=0 indicates SSID
-        ssid_elt = pkt[Dot11Elt:][0]
-        if ssid_elt.ID == 0:
-            ssid = ssid_elt.info.decode(errors="ignore").strip()
-
-        # If it’s an SSID broadcast (sometimes hidden)
-        if not ssid:
+    discovered_networks = {}
+    while len(discovered_networks) <= count:
+        pkt = sniff(iface=iface, store=False, count=count)
+        # either Beacon Frame or Prob Response Frame?
+        if pkt.haslayer(Dot11Beacon):
+            bssid = pkt[Dot11].addr2 # MAC address, i.e., BSSID
             ssid = "<Hidden>"
+            # Dot11Elt with ID=0 indicates SSID
+            if pkt[Dot11Elt:][0].ID == 0:
+                # parse SSID to a string
+                ssid = pkt[Dot11Elt:][0].info.decode(errors="ignore").strip()
+            stats_dict = pkt[Dot11Beacon].network_stats()
+            channel = stats_dict.get("channel", "N/A")
+            crypto = stats_dict.get("crypto", "N/A")
+            rssi = pkt.dBm_AntSignal if hasattr(pkt, 'dBm_AntSignal') else "N/A"
+            # BSSID not already in existing_bssids?
+            if bssid not in discovered_networks:
+               discovered_networks[bssid] = {"ssid": ssid,"bssid": bssid,"channel": channel,
+                                            "encryption": crypto,"rssi": rssi}
+            time.sleep(interval)
 
-        stats = pkt[Dot11Beacon].network_stats() if pkt.haslayer(Dot11Beacon) else {}
-        channel = stats.get("channel", "N/A")
-        crypto = stats.get("crypto", "N/A")
-        rssi = pkt.dBm_AntSignal if hasattr(pkt, 'dBm_AntSignal') else "N/A"
+    return discovered_networks
 
-        # Check if we already discovered this BSSID
-        existing_bssids = [net["bssid"] for net in discovered_networks]
-        if bssid not in existing_bssids:
-            discovered_networks.append({
-                "ssid": ssid,
-                "bssid": bssid,
-                "channel": channel,
-                "encryption": crypto,
-                "rssi": rssi
-            })
 
-            print(f"[+] New AP found:")
-            print(f"    SSID      : {ssid}")
-            print(f"    BSSID     : {bssid}")
-            print(f"    Channel   : {channel}")
-            print(f"    Encryption: {crypto}")
-            print(f"    RSSI: {rssi}")
-            print()
-            time.sleep(0.5)
+
 
 
 
@@ -57,7 +41,7 @@ def main():
     """
     interface = "wlan0mon"
     print(f"[*] Starting sniff on {interface}...")
-    sniff(iface=interface, prn=packet_handler, store=False)
+
 
 
 if __name__ == "__main__":
