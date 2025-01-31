66a89ac128688585c81108b26baf3f470f1bcba0
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-01-31 15:05:56 -0800

    editing setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,18 +1,40 @@
 import subprocess
+import sys
 
 def change_mode(iface, mode):
     """
     """
+    modes = ['managed', 'monitor']
+    if mode.lower() not in modes:
+        raise Exception(f'Unsupported interface mode {mode.lower()}')
     cmds = [
-    f"sudo ip link set {iface} down",
-    f"sudo iw dev {iface} set type {mode}", 
-    f"sudo ip link set {iface} up"
+        ["sudo", "ip", "link", "set", iface, "down"],
+        ["sudo", "iw", "dev", iface, "set", "type", mode.lower()],
+        ["sudo", "ip",  "link", "set", iface, "up"]
     ]
+
     try:
-        subprocess.run(cmds, shell=True, check=True, text=True)
+        for cmd in cmds:
+           subprocess.run(cmd, check=True, text=True)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
 
 
 
 
+def main():
+    if len(sys.argv) < 3:
+        print('Error: Missing arguments.')
+        print(f'Usage: {sys.argv[0]} <Interface> <Mode>')
+        print('Mode: managed, monitor')
+        sys.exit(1)
+
+    change_mode(sys.argv[1], sys.argv[2])
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
