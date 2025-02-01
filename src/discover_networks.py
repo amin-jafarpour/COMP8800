c75b4afb53e99548cc6a17d9fd4fb69c3f296264
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-01-31 19:19:45 -0800

    ...

--- src/discover_networks.py
+++ src/discover_networks.py
@@ -2,32 +2,59 @@ from scapy.all import sniff, Dot11, Dot11Beacon, Dot11ProbeResp, Dot11Elt
 import time
 import sys
 
+
+
+def bundle(pkt):
+   layers = [layer.__name__ for layer in pkt.layers()]
+   info = {}
+
+   for layer in layers:
+       pairs_lst = []
+
+       for field in pkt[layer].fields:
+          pairs_lst.append({field: pkt[layer].getfieldval(field)})
+
+       info[layer] = {layer: pairs_lst}
+
+   return info
+
+
+
+
+
 def process_packet(pkt, discovered_networks):
-   print(pkt)
-
-   if not (pkt.haslayer(Dot11Beacon)  or pkt.haslayer(Dot11ProbeResp) or pkt.haslayer(Dot11Elt)):
-       return
-   return
-   bssid = pkt[Dot11].addr2
-   if bssid in discovered_networks:
-       return
-
-   ssid = "<Hidden>"
-#   if pkt[Dot11Elt:][0].ID == 0: # Dot11Elt with ID=0 indicates SSID
-#      ssid = pkt[Dot11Elt:][0].info.decode(errors="ignore").strip()
    if pkt.haslayer(Dot11):
-      ssid = pkt[Dot11].info.decode(errors='ignore')
 
-   stats_dict = {}
-   if pkt.haslayer(Dot11Beacon):
-      stats_dict = pkt[Dot11Beacon].network_stats()
-   elif pkt.haslayer(Dot11ProbeResp):
-      stats_dict = pkt[Dot11ProbeResp].network_stats()
+      bssid = pkt[Dot11].addr2
+      if bssid not in discovered_networks:
+         pkt_info = bundle(pkt)
+         discovered_networks[bssid] = pkt_info
 
-   channel = stats_dict.get("channel", "N/A")
-   crypto = stats_dict.get("crypto", "N/A")
-   rssi = pkt.dBm_AntSignal if hasattr(pkt, 'dBm_AntSignal') else "N/A"
-   discovered_networks[bssid] = {"ssid": ssid,"bssid": bssid,"channel": channel,"encryption": crypto,"rssi": rssi}
+
+
+
+
+
+#    bssid = pkt[Dot11].addr2
+#    if bssid in discovered_networks:
+#        return
+#
+#    ssid = "<Hidden>"
+# #   if pkt[Dot11Elt:][0].ID == 0: # Dot11Elt with ID=0 indicates SSID
+# #      ssid = pkt[Dot11Elt:][0].info.decode(errors="ignore").strip()
+#    if pkt.haslayer(Dot11):
+#       ssid = pkt[Dot11].info.decode(errors='ignore')
+#
+#    stats_dict = {}
+#    if pkt.haslayer(Dot11Beacon):
+#       stats_dict = pkt[Dot11Beacon].network_stats()
+#    elif pkt.haslayer(Dot11ProbeResp):
+#       stats_dict = pkt[Dot11ProbeResp].network_stats()
+#
+#    channel = stats_dict.get("channel", "N/A")
+#    crypto = stats_dict.get("crypto", "N/A")
+#    rssi = pkt.dBm_AntSignal if hasattr(pkt, 'dBm_AntSignal') else "N/A"
+#    discovered_networks[bssid] = {"ssid": ssid,"bssid": bssid,"channel": channel,"encryption": crypto,"rssi": rssi}
 
 
 
@@ -38,6 +65,7 @@ def discover_networks(iface, count):
     while (len(discovered_networks) <= count):
         print(discovered_networks)
         sniff(iface=iface, count=count, store=False, prn= lambda pkt: process_packet(pkt, discovered_networks))
+        print(len(discovered_networks))
 
     return discovered_networks
 
@@ -49,3 +77,4 @@ def discover_networks(iface, count):
 
 print(discover_networks(sys.argv[1], int(sys.argv[2])))
 
+p
