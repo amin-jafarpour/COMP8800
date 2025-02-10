f8f2d3199dfa6d56c2659f6f44302e5a62c6ee85
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-02-09 18:59:56 -0800

    ...

--- src/radar.py
+++ src/radar.py
@@ -3,7 +3,7 @@ import math
 import random
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk, GdkPixbuf, cairo
+from gi.repository import Gtk, Gdk
 
 class RadarWindow(Gtk.Window):
     def __init__(self):
