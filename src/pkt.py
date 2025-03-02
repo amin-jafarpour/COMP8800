f064e33d70949c67f601eb8a7f76660170abafa8
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-03-02 15:36:57 -0800

    ...

--- src/pkt.py
+++ src/pkt.py
@@ -1,12 +1,3 @@
+class Pkt:
+    pass 
 
-import ipaddress 
-import scapy.all as scap
-
-
-iface_lst = scap.get_if_list()
-
-ip_addr_str = scap.get_if_addr(iface_lst[1])
-
-network = ipaddress.ip_network(ip_addr_str)
-
-x = network
