b432a3313bcaee66fd93a87d7fa43adbfece998f
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-03-02 14:03:55 -0800

    ...

--- src/pkt.py
+++ src/pkt.py
@@ -1,57 +1,7 @@
-# from scapy.all import ARP, Ether, srp
+# import os
 
-# class Pkt:
-#     pass
+# iface_lst = os.listdir('/sys/class/net')
 
+import socket
+print(socket.gethostbyname(socket.gethostname()))
 
-
-
-
-
-
-
-
-
-
-
-
-# def arp_scan(network):
-#     print(f"Scanning network: {network}")
-    
-#     # Create an ARP request packet
-#     arp = ARP(pdst=network)
-    
-#     # Create an Ethernet frame (broadcast)
-#     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-    
-#     # Combine Ethernet + ARP
-#     packet = ether / arp
-    
-#     # Send packet and receive responses
-#     answered, unanswered = srp(packet, timeout=2, verbose=False)
-    
-#     # Print results
-#     print("Live hosts:")
-#     for sent, received in answered:
-#         print(f"IP: {received.psrc}, MAC: {received.hwsrc}")
-
-# # Replace with your subnet (e.g., "192.168.1.1/24")
-# arp_scan("192.168.1.0/24")
-
-
-
-
-
-from scapy.all import get_if_addr, get_if_net, get_if_raw_hwaddr
-
-# Get interface name (e.g., eth0, wlan0)
-iface = "eth0"  # Change this to your interface
-
-# Get IP address
-ip_addr = get_if_addr(iface)
-
-# Get network mask
-netmask = get_if_net(iface)
-
-# Print CIDR notation
-print(f"{ip_addr}/{netmask}")
