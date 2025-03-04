c067a7c0f9f44e4be2a7ead4885cc1d71a4e5dfa
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-03-03 17:37:20 -0800

    ...

--- src/bt.py
+++ src/bt.py
@@ -48,7 +48,7 @@ class BT:
     @staticmethod
     def rfcomm_server():
         server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        server_sock.bind(("", bluetooth.PORT_ANY))
+        server_sock.bind(("00:E0:03:00:19:D1", bluetooth.PORT_ANY))
         server_sock.listen(1)
         port = server_sock.getsockname()[1]
         uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
