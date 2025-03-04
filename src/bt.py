b83fae89c90a5dc1b0731277d1b2ba41932e9c30
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-03-03 16:58:31 -0800

    ...

--- src/bt.py
+++ src/bt.py
@@ -16,30 +16,47 @@ class BT:
             devices_info.append({'addr': addr, 'name': name, 'cod': cod, 'services': services})
         return devices_info
 
+          
+          
+          
+          
+          
+# for svc in services:
+#     print("\nService Name:", svc["name"])
+#     print("    Host:       ", svc["host"])
+#     print("    Description:", svc["description"])
+#     print("    Provided By:", svc["provider"])
+#     print("    Protocol:   ", svc["protocol"])
+#     print("    channel/PSM:", svc["port"])
+#     print("    svc classes:", svc["service-classes"])
+#     print("    profiles:   ", svc["profiles"])
+#     print("    service id: ", svc["service-id"])  
             
-            
-    @staticmethod
-    def advertise():
-        server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        server_sock.bind(("", bluetooth.PORT_ANY)) 
-        server_sock.listen(1)
-        port = server_sock.getsockname()[1]
-        #host_mac = bluetooth.read_local_bdaddr()[0]
-        service_uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee" 
-        bluetooth.advertise_service(
-        server_sock, "MyBluetoothService",
-        service_id=service_uuid,
-        service_classes=[service_uuid, bluetooth.SERIAL_PORT_CLASS],  
-        profiles=[bluetooth.SERIAL_PORT_PROFILE],)
-        client_sock, client_info = server_sock.accept()
-        data = client_sock.recv(1024)
-        print(f"Received: {data.decode()}")
-        client_sock.close()
-        server_sock.close()
+    # @staticmethod
+    # def advertise():
+    #     server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    #     server_sock.bind(("", bluetooth.PORT_ANY)) 
+    #     server_sock.listen(1)
+    #     port = server_sock.getsockname()[1]
+    #     #host_mac = bluetooth.read_local_bdaddr()[0]
+    #     service_uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee" 
+    #     bluetooth.advertise_service(
+    #     server_sock, "MyBluetoothService",
+    #     service_id=service_uuid,
+    #     service_classes=[service_uuid, bluetooth.SERIAL_PORT_CLASS],  
+    #     profiles=[bluetooth.SERIAL_PORT_PROFILE],)
+    #     client_sock, client_info = server_sock.accept()
+    #     data = client_sock.recv(1024)
+    #     print(f"Received: {data.decode()}")
+    #     client_sock.close()
+    #     server_sock.close()
         
     
-    @staticmethod 
-    def dummy():
+  
+        
+
+        
+        
         
 
         
