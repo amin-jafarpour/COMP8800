76309b1667daef0f4bb870332e860c768caadd10
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-03-03 17:35:41 -0800

    ...

--- src/bt.py
+++ src/bt.py
@@ -18,12 +18,21 @@ class BT:
     
     
     @staticmethod
-    def l2capserver():    
+    def l2cap_server():    
+        # advertise_service(
+        # sock,
+        # name,
+        # service_id='',
+        # service_classes=[],
+        # profiles=[],
+        # provider='',
+        # description='',
+        # protocols=[])
         server_sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
         port = 0x1001
         server_sock.bind(("00:E0:03:00:19:D1", port))
         server_sock.listen(1)
-        uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ef"
+        uuid = "0000180a-0000-1000-8000-00805f9b34fb"
         bluetooth.advertise_service(server_sock, "SampleServerL2CAP", service_id=uuid, service_classes = [uuid])
         client_sock, address = server_sock.accept()
         print("Accepted connection from", address)
@@ -35,6 +44,35 @@ class BT:
             print("Data received:", str(data))
         client_sock.close()
         server_sock.close()    
+        
+    @staticmethod
+    def rfcomm_server():
+        server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        server_sock.bind(("", bluetooth.PORT_ANY))
+        server_sock.listen(1)
+        port = server_sock.getsockname()[1]
+        uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+        bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                            profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                            # protocols=[bluetooth.OBEX_UUID]
+                            )
+        print("Waiting for connection on RFCOMM channel", port)
+        client_sock, client_info = server_sock.accept()
+        print("Accepted connection from", client_info)
+        try:
+            while True:
+                data = client_sock.recv(1024)
+                if not data:
+                    break
+                print("Received", data)
+        except OSError:
+            pass
+
+        print("Disconnected.")
+        client_sock.close()
+        server_sock.close()
+        print("All done.")
         
 
           
@@ -52,25 +90,7 @@ class BT:
 #     print("    profiles:   ", svc["profiles"])
 #     print("    service id: ", svc["service-id"])  
             
-    # @staticmethod
-    # def advertise():
-    #     server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    #     server_sock.bind(("", bluetooth.PORT_ANY)) 
-    #     server_sock.listen(1)
-    #     port = server_sock.getsockname()[1]
-    #     #host_mac = bluetooth.read_local_bdaddr()[0]
-    #     service_uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee" 
-    #     bluetooth.advertise_service(
-    #     server_sock, "MyBluetoothService",
-    #     service_id=service_uuid,
-    #     service_classes=[service_uuid, bluetooth.SERIAL_PORT_CLASS],  
-    #     profiles=[bluetooth.SERIAL_PORT_PROFILE],)
-    #     client_sock, client_info = server_sock.accept()
-    #     data = client_sock.recv(1024)
-    #     print(f"Received: {data.decode()}")
-    #     client_sock.close()
-    #     server_sock.close()
-        
+
     
   
         
