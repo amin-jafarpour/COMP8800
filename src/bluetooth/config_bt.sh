2eda5740d9ced6a3d76e12e6e5f9ecf2baa188b0
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-03-09 16:51:59 -0700

    ...

--- src/bluetooth/config_bt.sh
+++ src/bluetooth/config_bt.sh
@@ -1,1 +1,5 @@
-sudo apt install bluez bluez-tools python3-bluez -y
+sudo apt install bluez bluez-tools python3-bluez rfkill -y
+sudo systemctl enable --now bluetooth
+sudo rfkill unblock bluetooth 
+
+
