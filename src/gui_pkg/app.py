ec25cb383e3aaf4ba654f5fe530d03b4641850d1
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-03-09 20:39:17 -0700

    ...

--- src/gui_pkg/app.py
+++ src/gui_pkg/app.py
@@ -32,6 +32,7 @@ def get_inet_net_scan():
     iface = request.args.get('iface', default='', type=str)
     net_count = request.args.get('net_count', default=1, type=int)
     net_lst = Inet.scan_networks(iface, net_count)
+    # BUG: Bytes decoding has issues. Fix it!
     return [{k: (v.decode(encoding='utf-32-be', errors='ignore') if isinstance(v, bytes) else v) for k, v in d.items()} for d in net_lst]
 
 
