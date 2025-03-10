38d86c458f99a37e51769a56316502e34c1ba5fe
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-03-09 20:16:20 -0700

    ...

--- src/config.sh
+++ src/config.sh
@@ -1,4 +1,4 @@
-sudo apt install bluez bluez-tools python3-bluez python3-gattlib rfkill libbluetooth-dev firmware-realtek python3-flask -y
+sudo apt install bluez bluez-tools python3-bluez python3-gattlib rfkill libbluetooth-dev firmware-realtek python3-flask iw -y
 sudo systemctl enable --now bluetooth
 sudo rfkill unblock bluetooth 
 
