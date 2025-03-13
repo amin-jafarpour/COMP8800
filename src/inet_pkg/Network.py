3436f237299f3124964cd26d918a1d9a8da58d2f
Amin Jafarpour <a.jafarpour2001@gmail.com>
2025-03-13 13:14:17 -0700

    ...

--- src/inet_pkg/Network.py
+++ src/inet_pkg/Network.py
@@ -2,9 +2,28 @@
 #password-protected network (WPA/WPA2)
 #enterprise network (WPA2-Enterprise) with both username and password
 
+
+
+sudo ip link set <interface> down
+
+sudo iw dev <interface> set type <mode>
+
 sudo ip link set <interface> up
 
 sudo iwlist <interface> scan | grep -E "SSID|Encryption"
+
+nmcli device wifi connect "<SSID>" ifname <interface> # open network 
+
+nmcli device wifi connect "<SSID>" password "<password>" ifname <interface>
+
+
+
+
+
+
+
+
+
 
 
 #sudo iwlist <interface> scan | grep -E "SSID|Encryption"
