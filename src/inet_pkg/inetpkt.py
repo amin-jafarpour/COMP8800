993d313fa212ce5d78421f393f47bf7ff21095e4
amin-jafarpour <a.jafarpour2001@gmail.com>
2025-03-13 19:11:36 -0700

    ...

--- src/inet_pkg/inetpkt.py
+++ src/inet_pkg/inetpkt.py
@@ -1,17 +1,19 @@
 from scapy.all import IP, ICMP, sr1
 import ipaddress
+from inet import Inet 
 
 class ICMP:
     @staticmethod
-    def ping_device(dst:str, timeout:int=2):
+    def ping_device(iface:str, dst:str, timeout:int=2):
         pkt = IP(dst=dst) / ICMP() 
         reply = sr1(pkt, timeout=timeout, verbose=False)
         return reply
 
     @staticmethod 
-    def ping_subnet(subnet:str, single_timeout:int):
-        replies = []
+    def ping_subnet(iface:str, single_timeout:int):
+        subnet = Inet.get_net_cidr(iface=iface)
         hosts = ipaddress.ip_network(subnet, strict=False).hosts()
+        replies = []
         for dst in hosts:
             reply = ICMP.ping_device(dst, single_timeout)
             replies.append(reply)
