24d1fc45f160eaa5e90f08c85bd7f4e09de037ff
amin-jafarpour <a.jafarpour2001@gmail.com>
2025-03-13 19:05:27 -0700

    ...

--- src/inet_pkg/inetpkt.py
+++ src/inet_pkg/inetpkt.py
@@ -1,6 +1,33 @@
+from scapy.all import IP, ICMP, sr1
+import ipaddress
+
+class ICMP:
+    @staticmethod
+    def ping_device(dst:str, timeout:int=2):
+        pkt = IP(dst=dst) / ICMP() 
+        reply = sr1(pkt, timeout=timeout, verbose=False)
+        return reply
+
+    @staticmethod 
+    def ping_subnet(subnet:str, single_timeout:int):
+        replies = []
+        hosts = ipaddress.ip_network(subnet, strict=False).hosts()
+        for dst in hosts:
+            reply = ICMP.ping_device(dst, single_timeout)
+            replies.append(reply)
+        return reply
 
 
-# Pinging via ICMP
+
+
+
+
+
+
+
+
+
+# Pinging via ICMP$
 
 # Port scaning: single, range, list of ports. 
 
